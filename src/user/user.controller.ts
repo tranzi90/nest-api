6fa32d1b8808f0282c19282830481ee0936c7c59
denis_levchenko <tranzi90@gmail.com>
2023-10-26 22:13:53 +0300

    add edit user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,15 @@
 import {
+  Body,
   Controller,
   Get,
+  Patch,
   UseGuards,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { AuthDto } from '../auth/dto';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
@@ -13,5 +17,13 @@ export class UserController {
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(
+    @GetUser('id') userId: number,
+    @Body() dto: EditUserDto,
+  ) {
+    return;
   }
 }
